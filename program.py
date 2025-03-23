88f8b1261117fcc408e1841e5fe67f2c28fa337a
HamzaElshafie <hamzaelshafie@outlook.com>
2025-03-23 23:29:00 +0000

    Setup full testing code.

--- program.py
+++ program.py
@@ -20,5 +20,26 @@ def test_op(BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM, causal, dtype=torch.float1
         .normal_(mean=0.0, std=0.5)
         .requires_grad_()
     )
- 
- 
+
+    softmax_scale = 1 / (HEAD_DIM**0.5) # QK^T/sqrt(HEAD_DIM)
+    d0 = torch.randn_like(Q) # Needed for the backward pass
+
+    # Reference (Vanilla) implementation
+    MASK = torch.tril(torch.ones((SEQ_LEN, SEQ_LEN), device="cuda"))
+    P = torch.matmul(Q, K.transpose(2,3)) * softmax_scale
+    if causal:
+        P[:, :, MASK == 0] = float("-inf")
+    P = torch.softmax(P.float(), dim=-1).half()
+
+    ref_0 = torch.matmul(P, V)
+    ref_0.backward(d0)
+    ref_dV, V.grad = V.grad.clone(), None
+    ref_dK, K.grad = K.grad.clone(), None
+    ref_dQ, Q.grad = Q.grad.clone(), None
+
+    # Triton attention
+    tri_out = TritonAttention.apply(Q, K, V, causal, softmax_scale).half()
+    tri_out.backward(d0)
+    tri_dV, V.grad = V.grad.clone(), None
+    tri_dK, K.grad = K.grad.clone(), None
+    tri_dQ, Q.grad = Q.grad.clone(), None
